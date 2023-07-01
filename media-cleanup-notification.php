b0129a09095bf1001045a8ad3711e8ba66bfd18c
Saiful Islam <42386363+saifislam01@users.noreply.github.com>
2023-07-02 00:35:57 +0600

    Update media-cleanup-notification.php

--- media-cleanup-notification.php
+++ media-cleanup-notification.php
@@ -1,64 +1,102 @@
 <?php
 /*
 Plugin Name: Media Cleanup Notification
-Plugin URI: http://portfolio.dainikajkermeghna.com
+Plugin URI: https://saifuldevs.com/
 Description: Provides notifications for deleting unused images from the media folder.
 Version: 1.0
 Author: Saiful Islam
-Author URI: http://portfolio.dainikajkermeghna.com
+Author URI: https://saifuldevs.com/
 */
 
-// Activate the plugin
-function media_cleanup_notification_activate() {
-    // add activation code here
+// Enqueue JavaScript and CSS files
+function unused_images_cleanup_enqueue_scripts() {
+    wp_enqueue_script('unused-images-cleanup-script', plugin_dir_url(__FILE__) . 'js/script.js', array('jquery'), '1.0', true);
+    wp_enqueue_style('unused-images-cleanup-style', plugin_dir_url(__FILE__) . 'css/style.css', array(), '1.0');
 }
-register_activation_hook( __FILE__, 'media_cleanup_notification_activate' );
+add_action('admin_enqueue_scripts', 'unused_images_cleanup_enqueue_scripts');
 
-function media_cleanup_notification_activate() {
-    add_option( 'media_cleanup_notification_last_sent', time() );
+// Add settings page
+function unused_images_cleanup_add_settings_page() {
+    add_options_page('Unused Images Cleanup', 'Unused Images Cleanup', 'manage_options', 'unused-images-cleanup', 'unused_images_cleanup_settings_page');
 }
+add_action('admin_menu', 'unused_images_cleanup_add_settings_page');
 
-function media_cleanup_notification_schedule() {
-    if ( ! wp_next_scheduled( 'media_cleanup_notification_cron' ) ) {
-        wp_schedule_event( time(), 'daily', 'media_cleanup_notification_cron' );
+// Display settings page
+function unused_images_cleanup_settings_page() {
+    if (!current_user_can('manage_options')) {
+        return;
     }
+    ?>
+    <div class="wrap">
+        <h1>Unused Images Cleanup</h1>
+        <p>Click the "Scan" button to scan your media folder for unused images.</p>
+        <button id="unused-images-scan-button" class="button">Scan</button>
+        <div id="unused-images-scan-results"></div>
+    </div>
+    <?php
 }
-add_action( 'wp', 'media_cleanup_notification_schedule' );
 
-function media_cleanup_notification_cron() {
-    // get the last time the notification was sent
-    $last_sent = get_option( 'media_cleanup_notification_last_sent' );
+// AJAX callback function for scanning unused images
+function unused_images_cleanup_scan_callback() {
+    if (!current_user_can('manage_options')) {
+        wp_send_json_error('Unauthorized');
+    }
 
-    // check if it's been more than a week since the last notification was sent
-    if ( time() - $last_sent >= 7 * 24 * 60 * 60 ) {
-        // get all the images in the media folder
-        $images = get_posts( array(
-            'post_type' => 'attachment',
-            'post_status' => 'inherit',
-            'posts_per_page' => -1,
-        ) );
+    $media_folder = wp_upload_dir()['basedir'];
 
-        // check each image to see if it's being used
-        foreach ( $images as $image ) {
-            if ( ! get_post_meta( $image->ID, '_wp_attachment_wpse1234', true ) ) {
-                // the image is not being used, send the cleanup notification
-                $subject = 'Unused images in your media folder';
-                $message = 'There are unused images in your media folder that can be safely deleted.';
-                wp_mail( get_option( 'admin_email' ), $subject, $message );
-                // update the last time the notification was sent
-                update_option( 'media_cleanup_notification_last_sent', time() );
-                break; // stop checking images once an unused image is found
+    $attachments = get_posts(array(
+        'post_type'      => 'attachment',
+        'posts_per_page' => -1,
+        'post_status'    => 'inherit',
+    ));
+
+    $used_images = array();
+    foreach ($attachments as $attachment) {
+        $attachment_metadata = wp_get_attachment_metadata($attachment->ID);
+        if ($attachment_metadata) {
+            if (isset($attachment_metadata['file'])) {
+                $file = $attachment_metadata['file'];
+                $used_images[] = $media_folder . '/' . $file;
+            }
+            if (isset($attachment_metadata['sizes'])) {
+                foreach ($attachment_metadata['sizes'] as $size) {
+                    if (isset($size['file'])) {
+                        $file = $size['file'];
+                        $used_images[] = $media_folder . '/' . $file;
+                    }
+                }
             }
         }
     }
+
+    $all_images = glob($media_folder . '/*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+    $unused_images = array_diff($all_images, $used_images);
+
+    wp_send_json_success(array(
+        'total_images'   => count($all_images),
+        'unused_images'  => count($unused_images),
+        'unused_list'    => $unused_images,
+    ));
 }
+add_action('wp_ajax_unused_images_cleanup_scan', 'unused_images_cleanup_scan_callback');
 
-// Deactivate the plugin
-function media_cleanup_notification_deactivate() {
-    wp_clear_scheduled_hook( 'media_cleanup_notification_cron' );
-    delete_option( 'media_cleanup_notification_last_sent' );
+// AJAX callback function for deleting unused images
+function unused_images_cleanup_delete_callback() {
+    if (!current_user_can('manage_options')) {
+        wp_send_json_error('Unauthorized');
+    }
+
+    $images_to_delete = $_POST['images'];
+
+    $deleted_count = 0;
+    foreach ($images_to_delete as $image) {
+        if (file_exists($image)) {
+            if (unlink($image)) {
+                $deleted_count++;
+            }
+        }
+    }
+
+    wp_send_json_success(array('deleted_count' => $deleted_count));
 }
-register_deactivation_hook( __FILE__, 'media_cleanup_notification_de');
-
-
-
+add_action('wp_ajax_unused_images_cleanup_delete', 'unused_images_cleanup_delete_callback');
